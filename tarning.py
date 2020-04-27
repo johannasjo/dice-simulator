f0634e7947859800becfdf25c64f7f6a499f85f7
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2020-04-27 08:12:05 +0200

    added comments and made the output  more clear

--- tarning.py
+++ tarning.py
@@ -1,18 +1,3 @@
-'''
-• Be användaren om ett val av hur många tärningar som skall kastas och ta emot 
-  den informationen när den trycks in. Minst 1 och max 4 tärningar skall väljas av användaren.
-
-• Programmet slumpar resultat för varje enskild tärning, en i taget, 
-  och skriver ut detta på skärmen tillsammans med summan av slagna tärningar hittills.
-
-• Om någon tärning får resultatet 1 så adderas detta inte till summan, 
-  utan två nya tärningar slås. Detta ska framgå av text på skärmen när/om det sker. 
-  Så länge någon tärning får resultatet 1 upprepas denna procedur.
-
-• När alla tärningar är slagna skrivs totalsumman samt antal tärningsslag ut på skärmen.
-
-• Spelaren får alternativ att börja om eller avsluta programmet.'''
-
 import random
 
 def print_line():
@@ -20,7 +5,7 @@ def print_line():
 
 def introduction():
     name = "Johanna Sjöstrand "
-    date = "24/4/2020"
+    date = "27/4/2020"
 
     print (name + date)
     print_line()
@@ -36,6 +21,7 @@ def choose_amount_of_die():
 
         else:
             amount_of_die = int(amount_of_die)
+           
 
         if not (amount_of_die >= 1 and amount_of_die <= 4):
             print ("Fel val av antal tärningar! Försök igen!")
@@ -45,41 +31,41 @@ def choose_amount_of_die():
     return amount_of_die
 
 def throw_dice():
+# Will randomize number of the dice when called
     return random.randint(1, 6)
 
 
 def play_dice(amount_of_die):
-    '''
-    1. Programmet slumpar resultat för varje enskild tärning, en i taget, 
-      och skriver ut detta på skärmen tillsammans med summan av slagna tärningar 
-      hittills. 
-    2. Om någon tärning får resultatet 1 så adderas detta inte till summan, 
-       utan två nya tärningar slås. Detta ska framgå av text på skärmen när/om det sker. 
-       Så länge någon tärning får resultatet 1 upprepas denna procedur.''' 
+ # Will ask to throw dice until the input number of die is reached, 
+ # add 2 die if the value is 1 and return and print the result
     total_sum = 0 
     throw_number = 0
     while throw_number < amount_of_die:
         input("Kasta tärning? Tryck enter när du är redo!")
+        print_line()
         value_of_dice = throw_dice()
         throw_number += 1
         
         if value_of_dice == 1:
             amount_of_die += 2
-            print ("Du slog en 1:a, då får du slå två nya tärningar!")
+            print ("Du slog en 1:a, då får du slå två nya tärningar!") 
+            print ("Ettan räknas inte in i slutsumman.")
+            print_line()
         
         else:
             total_sum += value_of_dice 
             print("Du kastade: " + str(value_of_dice))
-
+       
         print_line() 
         print("Summan av kastade tärningar är: " + str(total_sum))
         print_line() 
     return throw_number, total_sum
-    #När alla tärningar är slagna skrivs totalsumman samt antal tärningsslag ut på skärmen.
+    
   
 
 
-#Spelaren får alternativ att börja om eller avsluta programmet.
+#Function will ask player if they want to play again when called 
+#Can only answer 'j' or 'n'
 
 def ask_continue():
     while True:
@@ -92,8 +78,6 @@ def ask_continue():
 def main():
     introduction()
     while True:
-    
-
         amount_of_die = choose_amount_of_die()
         amount_of_throws, total_sum = play_dice(amount_of_die)
         print("Det totala antalet tärningsslag blev: " + str(amount_of_throws))
@@ -102,20 +86,10 @@ def main():
             
         answer = ask_continue()
         if not answer:
-            break
-
-
-
-    
-            
+            break         
           
     print("Tack för den här spelomgången! Kom gärna tillbaka imorgon!")
      
          
-
-
-    
-
-
 if __name__ == "__main__":
-    main() 
+    main()
